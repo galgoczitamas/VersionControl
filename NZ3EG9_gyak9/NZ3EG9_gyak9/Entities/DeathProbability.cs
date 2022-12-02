cfdaab1c047b929e1987c3f3aec380b93cbe4bf4
Tamás Ferenc Galgóczi <tamas.galgoczi@stud.uni-corvinus.hu>
2022-12-02 13:08:08 +0100

    .

---
+++ NZ3EG9_gyak9/NZ3EG9_gyak9/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZ3EG9_gyak9.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
+        public double P { get; set; }
+    }
+}
